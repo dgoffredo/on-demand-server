986b4977aa844dc74939883825bd2304184db8a5
David Goffredo <dmgoffredo@gmail.com>
2024-04-16 10:38:52 -0400

    add some delay to dummy.js, to test connection retries

--- dummy.js
+++ dummy.js
@@ -2,23 +2,26 @@ const net = require('node:net');
 const process = require('node:process');
 const {per_line} = require('./per-line.js');
 
-const server = net.createServer(sock => {
-	sock.write('Echo server\n');
-	per_line({stream: sock, callback: line => sock.write('echo line> ' + line)});
-});
+const startup_delay_ms = 2000;
 
-const host = '127.0.0.1';
-const port = 1337;
-server.listen(port, host, () => {
-  console.log(`dummy listening on ${host}:${port}`);
-});
+setTimeout(() => {
+  const server = net.createServer(sock => {
+    sock.write('Echo server\n');
+    per_line({stream: sock, callback: line => sock.write('echo line> ' + line)});
+  });
 
-function handle_signal(signal) {
-  console.log(`dummy received signal ${signal}.`);
-  // Nope, close harder... server.close(() => process.exit());
-  process.exit();
-}
+  const host = '127.0.0.1';
+  const port = 1337;
+  server.listen(port, host, () => {
+    console.log(`dummy listening on ${host}:${port}`);
+  });
 
-process.on('SIGINT', handle_signal);
-process.on('SIGTERM', handle_signal);
+  function handle_signal(signal) {
+    console.log(`dummy received signal ${signal}.`);
+    // Close harder than `server.close(() => process.exit());`
+    process.exit();
+  }
 
+  process.on('SIGINT', handle_signal);
+  process.on('SIGTERM', handle_signal);
+}, startup_delay_ms);
